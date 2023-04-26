3ae3539152ddc4a6d528b2ab585f7fb3226bf0bb
Anuj Khurana <aanujkhurana@gmail.com>
2023-04-26 14:48:20 +1000

    Initial Commit

---
+++ FavouritePlaces/FavouritePlacesApp.swift
@@ -0,0 +1,17 @@
+//
+//  FavouritePlacesApp.swift
+//  FavouritePlaces
+//
+//  Created by Anuj Khurana on 26/4/2023.
+//
+
+import SwiftUI
+
+@main
+struct FavouritePlacesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
